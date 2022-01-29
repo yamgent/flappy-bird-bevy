826d7c9f144f346790ac46b9d902510ec3881927
Tan Wang Leng <tanwangleng@outlook.com>
2022-01-29 22:39:28 +0800

    Kill player if they went out of windows bound

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use bevy::prelude::*;
 #[derive(Component)]
 struct Player {
     y_velocity: f32,
+    dead: bool,
 }
 
 fn main() {
@@ -20,21 +21,35 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             texture: asset_server.load("player.png"),
             ..Default::default()
         })
-        .insert(Player { y_velocity: 0.0 });
+        .insert(Player {
+            y_velocity: 0.0,
+            dead: false,
+        });
 }
 
 fn simulate_player_gravity(
+    windows: Res<Windows>,
     time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<(&mut Player, &mut Transform)>,
 ) {
     let (mut player, mut transform) = query.single_mut();
 
-    if keyboard_input.just_pressed(KeyCode::Space) {
-        player.y_velocity = 5.0;
-    } else {
-        player.y_velocity -= time.delta().as_secs_f32() * 9.81;
-    }
+    if !player.dead {
+        if keyboard_input.just_pressed(KeyCode::Space) {
+            player.y_velocity = 5.0;
+        } else {
+            player.y_velocity -= time.delta().as_secs_f32() * 9.81;
+        }
 
-    transform.translation.y += player.y_velocity;
+        transform.translation.y += player.y_velocity;
+
+        let window = windows.get_primary().unwrap();
+
+        let (min_y, max_y) = (-window.height() as f32 / 2.0, window.height() as f32 / 2.0);
+
+        if transform.translation.y < min_y || transform.translation.y > max_y {
+            player.dead = true;
+        }
+    }
 }
