529dc97d1ce110a7a7d61eb341a3928c98eaa5ef
Tan Wang Leng <tanwangleng@outlook.com>
2022-01-29 22:04:13 +0800

    Add white sprite

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        transform: Transform {
+            translation: Vec3::new(0.0, 0.0, 0.0),
+            scale: Vec3::new(64.0, 64.0, 0.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
